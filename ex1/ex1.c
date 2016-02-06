7080c1009e217e55b679b4fe08fa822f7b184441
Dominik Magdaleński <dominik0294@gmail.com>
2016-02-06 18:58:29 +0100

    first commit with ex1

---
+++ ex1/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    puts("This is the second line.");
+    puts("blablablabalbalbalablablablabalbalbalablablabalbalbalablbalbalblablablablablablabla.........");
+    puts("Print out 5 more lines of text or something more complex than hello world.");
+    puts("Run man 3 puts and read about this functino and many others.");
+    puts("This is the end...");
+
+    return 0;
+}
