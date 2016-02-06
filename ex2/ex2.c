49a8ca5b9790d3627b9bd3a63c4779477cbe5ea2
Dominik Magdaleński <dominik0294@gmail.com>
2016-02-06 22:28:39 +0100

    Added ex2

---
+++ ex2/ex2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int arg, char * argv[])
+{
+	puts("Hello world.");
+	puts("This is the second line.");
+	puts("basbdoiasbdiobasoibdoiasbdiobasiobdoiasbdoibasoidiasodbasoibdaoisbdas...");
+	puts("Print out 5 more lines of text or somthing more complex tha hello world.");
+	puts("Run man 3 puts and read about this function and many others.");
+	puts("This is the end...");
+}
